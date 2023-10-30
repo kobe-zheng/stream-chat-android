92f2e6254dc1426a2a3c9376d327afacf701aba1
Kobe <kobe.zheng@priceline.com>
2023-10-30 14:19:52 -0400

    remove unused methods in messagelist view model

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/viewmodel/messages/MessageListViewModel.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/viewmodel/messages/MessageListViewModel.kt
@@ -24,14 +24,10 @@ import io.getstream.chat.android.models.Channel
 import io.getstream.chat.android.models.ConnectionState
 import io.getstream.chat.android.models.Message
 import io.getstream.chat.android.models.User
-import io.getstream.chat.android.state.plugin.state.channel.thread.ThreadState
-import io.getstream.chat.android.ui.common.feature.messages.list.DateSeparatorHandler
 import io.getstream.chat.android.ui.common.feature.messages.list.MessageListController
-import io.getstream.chat.android.ui.common.feature.messages.list.MessagePositionHandler
 import io.getstream.chat.android.ui.common.state.messages.MessageAction
 import io.getstream.chat.android.ui.common.state.messages.MessageMode
 import io.getstream.chat.android.ui.common.state.messages.list.DeletedMessageVisibility
-import io.getstream.chat.android.ui.common.state.messages.list.GiphyAction
 import io.getstream.chat.android.ui.common.state.messages.list.MessageFooterVisibility
 import io.getstream.chat.android.ui.common.state.messages.list.MessageItemState
 import io.getstream.chat.android.ui.common.state.messages.list.MessageListItemState
@@ -42,7 +38,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 
 /**
  * ViewModel responsible for handling all the business logic & state for the list of messages.
@@ -213,178 +208,178 @@ public class MessageListViewModel(
         messageListController.selectMessage(message)
     }
 
-    /**
-     * Triggered when the user taps on and selects message reactions.
-     *
-     * @param message The message that contains the reactions.
-     */
-    public fun selectReactions(message: Message?) {
-        messageListController.selectReactions(message)
-    }
+    // /**
+    //  * Triggered when the user taps on and selects message reactions.
+    //  *
+    //  * @param message The message that contains the reactions.
+    //  */
+    // public fun selectReactions(message: Message?) {
+    //     messageListController.selectReactions(message)
+    // }
 
-    /**
-     * Triggered when the user taps the show more reactions button.
-     *
-     * @param message The selected message.
-     */
-    public fun selectExtendedReactions(message: Message?) {
-        messageListController.selectReactions(message)
-    }
+    // /**
+    //  * Triggered when the user taps the show more reactions button.
+    //  *
+    //  * @param message The selected message.
+    //  */
+    // public fun selectExtendedReactions(message: Message?) {
+    //     messageListController.selectReactions(message)
+    // }
 
-    /**
-     *  Changes the current messageMode to be [Thread] with [ThreadState] and Loads thread data using ChatClient
-     *  directly. The data is observed by using [ThreadState].
-     *
-     * @param message The selected message with a thread.
-     */
-    public fun openMessageThread(message: Message) {
-        viewModelScope.launch {
-            messageListController.enterThreadMode(message)
-        }
-    }
+    // /**
+    //  *  Changes the current messageMode to be [Thread] with [ThreadState] and Loads thread data using ChatClient
+    //  *  directly. The data is observed by using [ThreadState].
+    //  *
+    //  * @param message The selected message with a thread.
+    //  */
+    // public fun openMessageThread(message: Message) {
+    //     viewModelScope.launch {
+    //         messageListController.enterThreadMode(message)
+    //     }
+    // }
 
-    /**
-     * Used to dismiss a specific message action, such as delete, reply, edit or something similar.
-     *
-     * @param messageAction The action to dismiss.
-     */
-    public fun dismissMessageAction(messageAction: MessageAction) {
-        messageListController.dismissMessageAction(messageAction)
-    }
+    // /**
+    //  * Used to dismiss a specific message action, such as delete, reply, edit or something similar.
+    //  *
+    //  * @param messageAction The action to dismiss.
+    //  */
+    // public fun dismissMessageAction(messageAction: MessageAction) {
+    //     messageListController.dismissMessageAction(messageAction)
+    // }
 
-    /**
-     * Dismisses all message actions, when we cancel them in the rest of the UI.
-     */
-    public fun dismissAllMessageActions() {
-        messageListController.dismissAllMessageActions()
-    }
+    // /**
+    //  * Dismisses all message actions, when we cancel them in the rest of the UI.
+    //  */
+    // public fun dismissAllMessageActions() {
+    //     messageListController.dismissAllMessageActions()
+    // }
 
-    /**
-     * Triggered when the user selects a new message action, in the message overlay.
-     *
-     * We first remove the overlay, after which we consume the event and based on the type of the event,
-     * we do different things, such as starting a thread & loading thread data, showing delete or flag
-     * events and dialogs, copying the message, muting users and more.
-     *
-     * @param messageAction The action the user chose.
-     */
-    public fun performMessageAction(messageAction: MessageAction) {
-        viewModelScope.launch {
-            messageListController.performMessageAction(messageAction)
-        }
-    }
+    // /**
+    //  * Triggered when the user selects a new message action, in the message overlay.
+    //  *
+    //  * We first remove the overlay, after which we consume the event and based on the type of the event,
+    //  * we do different things, such as starting a thread & loading thread data, showing delete or flag
+    //  * events and dialogs, copying the message, muting users and more.
+    //  *
+    //  * @param messageAction The action the user chose.
+    //  */
+    // public fun performMessageAction(messageAction: MessageAction) {
+    //     viewModelScope.launch {
+    //         messageListController.performMessageAction(messageAction)
+    //     }
+    // }
 
-    /**
-     * Removes the delete actions from our [messageActions], as well as the overlay, before deleting
-     * the selected message.
-     *
-     * @param message Message to delete.
-     */
-    @JvmOverloads
-    @Suppress("ConvertArgumentToSet")
-    public fun deleteMessage(message: Message, hard: Boolean = false) {
-        messageListController.deleteMessage(message, hard)
-    }
+    // /**
+    //  * Removes the delete actions from our [messageActions], as well as the overlay, before deleting
+    //  * the selected message.
+    //  *
+    //  * @param message Message to delete.
+    //  */
+    // @JvmOverloads
+    // @Suppress("ConvertArgumentToSet")
+    // public fun deleteMessage(message: Message, hard: Boolean = false) {
+    //     messageListController.deleteMessage(message, hard)
+    // }
 
-    /**
-     * Removes the flag actions from our [messageActions], as well as the overlay, before flagging
-     * the selected message.
-     *
-     * @param message Message to delete.
-     */
-    @Suppress("ConvertArgumentToSet")
-    public fun flagMessage(message: Message) {
-        messageListController.flagMessage(message)
-    }
+    // /**
+    //  * Removes the flag actions from our [messageActions], as well as the overlay, before flagging
+    //  * the selected message.
+    //  *
+    //  * @param message Message to delete.
+    //  */
+    // @Suppress("ConvertArgumentToSet")
+    // public fun flagMessage(message: Message) {
+    //     messageListController.flagMessage(message)
+    // }
 
-    /**
-     * Mutes the given user inside this channel.
-     *
-     * @param userId The ID of the user to be muted.
-     * @param timeout The period of time for which the user will
-     * be muted, expressed in minutes. A null value signifies that
-     * the user will be muted for an indefinite time.
-     */
-    public fun muteUser(userId: String, timeout: Int? = null) {
-        messageListController.muteUser(userId, timeout)
-    }
+    // /**
+    //  * Mutes the given user inside this channel.
+    //  *
+    //  * @param userId The ID of the user to be muted.
+    //  * @param timeout The period of time for which the user will
+    //  * be muted, expressed in minutes. A null value signifies that
+    //  * the user will be muted for an indefinite time.
+    //  */
+    // public fun muteUser(userId: String, timeout: Int? = null) {
+    //     messageListController.muteUser(userId, timeout)
+    // }
 
-    /**
-     * Unmutes the given user inside this channel.
-     *
-     * @param userId The ID of the user to be unmuted.
-     */
-    public fun unmuteUser(userId: String) {
-        messageListController.unmuteUser(userId)
-    }
+    // /**
+    //  * Unmutes the given user inside this channel.
+    //  *
+    //  * @param userId The ID of the user to be unmuted.
+    //  */
+    // public fun unmuteUser(userId: String) {
+    //     messageListController.unmuteUser(userId)
+    // }
 
-    /**
-     * Bans the given user inside this channel.
-     *
-     * @param userId The ID of the user to be banned.
-     * @param reason The reason for banning the user.
-     * @param timeout The period of time for which the user will
-     * be banned, expressed in minutes. A null value signifies that
-     * the user will be banned for an indefinite time.
-     */
-    public fun banUser(
-        userId: String,
-        reason: String? = null,
-        timeout: Int? = null,
-    ) {
-        messageListController.banUser(userId = userId, reason = reason, timeout = timeout)
-    }
+    // /**
+    //  * Bans the given user inside this channel.
+    //  *
+    //  * @param userId The ID of the user to be banned.
+    //  * @param reason The reason for banning the user.
+    //  * @param timeout The period of time for which the user will
+    //  * be banned, expressed in minutes. A null value signifies that
+    //  * the user will be banned for an indefinite time.
+    //  */
+    // public fun banUser(
+    //     userId: String,
+    //     reason: String? = null,
+    //     timeout: Int? = null,
+    // ) {
+    //     messageListController.banUser(userId = userId, reason = reason, timeout = timeout)
+    // }
 
-    /**
-     * Unbans the given user inside this channel.
-     *
-     * @param userId The ID of the user to be unbanned.
-     */
-    public fun unbanUser(userId: String) {
-        messageListController.unbanUser(userId)
-    }
+    // /**
+    //  * Unbans the given user inside this channel.
+    //  *
+    //  * @param userId The ID of the user to be unbanned.
+    //  */
+    // public fun unbanUser(userId: String) {
+    //     messageListController.unbanUser(userId)
+    // }
 
-    /**
-     * Shadow bans the given user inside this channel.
-     *
-     * @param userId The ID of the user to be shadow banned.
-     * @param reason The reason for shadow banning the user.
-     * @param timeout The period of time for which the user will
-     * be shadow banned, expressed in minutes. A null value signifies that
-     * the user will be shadow banned for an indefinite time.
-     */
-    public fun shadowBanUser(
-        userId: String,
-        reason: String? = null,
-        timeout: Int? = null,
-    ) {
-        messageListController.shadowBanUser(userId = userId, reason = reason, timeout = timeout)
-    }
+    // /**
+    //  * Shadow bans the given user inside this channel.
+    //  *
+    //  * @param userId The ID of the user to be shadow banned.
+    //  * @param reason The reason for shadow banning the user.
+    //  * @param timeout The period of time for which the user will
+    //  * be shadow banned, expressed in minutes. A null value signifies that
+    //  * the user will be shadow banned for an indefinite time.
+    //  */
+    // public fun shadowBanUser(
+    //     userId: String,
+    //     reason: String? = null,
+    //     timeout: Int? = null,
+    // ) {
+    //     messageListController.shadowBanUser(userId = userId, reason = reason, timeout = timeout)
+    // }
 
-    /**
-     * Removes the shadow ban for the given user inside
-     * this channel.
-     *
-     * @param userId The ID of the user for which the shadow
-     * ban is removed.
-     */
-    public fun removeShadowBanFromUser(userId: String) {
-        messageListController.removeShadowBanFromUser(userId)
-    }
+    // /**
+    //  * Removes the shadow ban for the given user inside
+    //  * this channel.
+    //  *
+    //  * @param userId The ID of the user for which the shadow
+    //  * ban is removed.
+    //  */
+    // public fun removeShadowBanFromUser(userId: String) {
+    //     messageListController.removeShadowBanFromUser(userId)
+    // }
 
-    /**
-     * Leaves the thread we're in and resets the state of the messageMode and both of the [MessageListState]s.
-     */
-    public fun leaveThread() {
-        messageListController.enterNormalMode()
-    }
+    // /**
+    //  * Leaves the thread we're in and resets the state of the messageMode and both of the [MessageListState]s.
+    //  */
+    // public fun leaveThread() {
+    //     messageListController.enterNormalMode()
+    // }
 
-    /**
-     * Resets the [MessageListState]s, to remove the message overlay, by setting 'selectedMessage' to null.
-     */
-    public fun removeOverlay() {
-        messageListController.removeOverlay()
-    }
+    // /**
+    //  * Resets the [MessageListState]s, to remove the message overlay, by setting 'selectedMessage' to null.
+    //  */
+    // public fun removeOverlay() {
+    //     messageListController.removeOverlay()
+    // }
 
     /**
      * Clears the [NewMessageState] from our UI state, after the user taps on the "Scroll to bottom"
@@ -404,14 +399,14 @@ public class MessageListViewModel(
         return messageListController.getMessageFromListStateById(messageId)
     }
 
-    /**
-     * Executes one of the actions for the given ephemeral giphy message.
-     *
-     * @param action The action to be executed.
-     */
-    public fun performGiphyAction(action: GiphyAction) {
-        messageListController.performGiphyAction(action)
-    }
+    // /**
+    //  * Executes one of the actions for the given ephemeral giphy message.
+    //  *
+    //  * @param action The action to be executed.
+    //  */
+    // public fun performGiphyAction(action: GiphyAction) {
+    //     messageListController.performGiphyAction(action)
+    // }
 
     /**
      * Scrolls to message if in list otherwise get the message from backend. Does not work for threads.
@@ -443,63 +438,63 @@ public class MessageListViewModel(
         messageListController.scrollToBottom(messageLimit, scrollToBottom)
     }
 
-    /**
-     * Sets a handler which determines the position of a message inside a group.
-     *
-     * @param messagePositionHandler The handler to use.
-     */
-    public fun setMessagePositionHandler(messagePositionHandler: MessagePositionHandler) {
-        messageListController.setMessagePositionHandler(messagePositionHandler)
-    }
+    // /**
+    //  * Sets a handler which determines the position of a message inside a group.
+    //  *
+    //  * @param messagePositionHandler The handler to use.
+    //  */
+    // public fun setMessagePositionHandler(messagePositionHandler: MessagePositionHandler) {
+    //     messageListController.setMessagePositionHandler(messagePositionHandler)
+    // }
 
-    /**
-     * Sets the date separator handler which determines when to add date separators.
-     * By default, a date separator will be added if the difference between two messages' dates is greater than 4h.
-     *
-     * @param dateSeparatorHandler The handler to use. If null, the messages list won't contain date separators.
-     */
-    public fun setDateSeparatorHandler(dateSeparatorHandler: DateSeparatorHandler?) {
-        messageListController.setDateSeparatorHandler(dateSeparatorHandler)
-    }
+    // /**
+    //  * Sets the date separator handler which determines when to add date separators.
+    //  * By default, a date separator will be added if the difference between two messages' dates is greater than 4h.
+    //  *
+    //  * @param dateSeparatorHandler The handler to use. If null, the messages list won't contain date separators.
+    //  */
+    // public fun setDateSeparatorHandler(dateSeparatorHandler: DateSeparatorHandler?) {
+    //     messageListController.setDateSeparatorHandler(dateSeparatorHandler)
+    // }
 
-    /**
-     * Sets the thread date separator handler which determines when to add date separators inside the thread.
-     *
-     * @param threadDateSeparatorHandler The handler to use. If null, the thread messages list won't contain date
-     * separators.
-     */
-    public fun setThreadDateSeparatorHandler(threadDateSeparatorHandler: DateSeparatorHandler?) {
-        messageListController.setThreadDateSeparatorHandler(threadDateSeparatorHandler)
-    }
+    // /**
+    //  * Sets the thread date separator handler which determines when to add date separators inside the thread.
+    //  *
+    //  * @param threadDateSeparatorHandler The handler to use. If null, the thread messages list won't contain date
+    //  * separators.
+    //  */
+    // public fun setThreadDateSeparatorHandler(threadDateSeparatorHandler: DateSeparatorHandler?) {
+    //     messageListController.setThreadDateSeparatorHandler(threadDateSeparatorHandler)
+    // }
 
-    /**
-     * Sets the value used to determine if message footer content is shown.
-     * @see MessageFooterVisibility
-     *
-     * @param messageFooterVisibility Changes the visibility of message footers.
-     */
-    public fun setMessageFooterVisibility(messageFooterVisibility: MessageFooterVisibility) {
-        messageListController.setMessageFooterVisibility(messageFooterVisibility)
-    }
+    // /**
+    //  * Sets the value used to determine if message footer content is shown.
+    //  * @see MessageFooterVisibility
+    //  *
+    //  * @param messageFooterVisibility Changes the visibility of message footers.
+    //  */
+    // public fun setMessageFooterVisibility(messageFooterVisibility: MessageFooterVisibility) {
+    //     messageListController.setMessageFooterVisibility(messageFooterVisibility)
+    // }
 
-    /**
-     * Sets the value used to filter deleted messages.
-     * @see DeletedMessageVisibility
-     *
-     * @param deletedMessageVisibility Changes the visibility of deleted messages.
-     */
-    public fun setDeletedMessageVisibility(deletedMessageVisibility: DeletedMessageVisibility) {
-        messageListController.setDeletedMessageVisibility(deletedMessageVisibility)
-    }
+    // /**
+    //  * Sets the value used to filter deleted messages.
+    //  * @see DeletedMessageVisibility
+    //  *
+    //  * @param deletedMessageVisibility Changes the visibility of deleted messages.
+    //  */
+    // public fun setDeletedMessageVisibility(deletedMessageVisibility: DeletedMessageVisibility) {
+    //     messageListController.setDeletedMessageVisibility(deletedMessageVisibility)
+    // }
 
-    /**
-     * Sets whether the system messages should be visible.
-     *
-     * @param areSystemMessagesVisible Whether system messages should be visible or not.
-     */
-    public fun setSystemMessageVisibility(areSystemMessagesVisible: Boolean) {
-        messageListController.setSystemMessageVisibility(areSystemMessagesVisible)
-    }
+    // /**
+    //  * Sets whether the system messages should be visible.
+    //  *
+    //  * @param areSystemMessagesVisible Whether system messages should be visible or not.
+    //  */
+    // public fun setSystemMessageVisibility(areSystemMessagesVisible: Boolean) {
+    //     messageListController.setSystemMessageVisibility(areSystemMessagesVisible)
+    // }
 
     /**
      * Clears the [MessageListController] coroutine scope.
