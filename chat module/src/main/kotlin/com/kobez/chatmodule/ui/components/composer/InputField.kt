79a4ad751d998af1419c1626bbfb2dfc0c4763f6
Kobe <kobe.zheng@priceline.com>
2023-11-02 22:04:09 -0400

    add composer components

--- chat module/src/main/kotlin/com/kobez/chatmodule/ui/components/composer/InputField.kt
+++ chat module/src/main/kotlin/com/kobez/chatmodule/ui/components/composer/InputField.kt
@@ -1,4 +1,106 @@
 package com.kobez.chatmodule.ui.components.composer
 
-class InputField {
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.unit.dp
+import com.kobez.chatmodule.theme.ChatTheme
+
+/**
+ * Custom input field that we use for our UI. It's fairly simple - shows a basic input with clipped
+ * corners and a border stroke, with some extra padding on each side.
+ *
+ * Within it, we allow for custom decoration, so that the user can define what the input field looks like
+ * when filled with content.
+ *
+ * @param value The current input value.
+ * @param onValueChange Handler when the value changes as the user types.
+ * @param modifier Modifier for styling.
+ * @param enabled If the Composable is enabled for text input or not.
+ * @param maxLines The number of lines that are allowed in the input, no limit by default.
+ * @param border The [BorderStroke] that will appear around the input field.
+ * @param innerPadding The padding inside the input field, around the label or input.
+ * @param keyboardOptions The [KeyboardOptions] to be applied to the input.
+ * @param decorationBox Composable function that represents the input field decoration as it's filled with content.
+ */
+@Composable
+public fun InputField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    border: BorderStroke = BorderStroke(1.dp, Color.Gray),
+    innerPadding: PaddingValues = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+    keyboardOptions: KeyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
+    decorationBox: @Composable (innerTextField: @Composable () -> Unit) -> Unit,
+) {
+    var textFieldValueState by remember { mutableStateOf(TextFieldValue(text = value)) }
+
+    // Workaround to move cursor to the end after selecting a suggestion
+    val selection = if (textFieldValueState.isCursorAtTheEnd()) {
+        TextRange(value.length)
+    } else {
+        textFieldValueState.selection
+    }
+
+    val textFieldValue = textFieldValueState.copy(
+        text = value,
+        selection = selection,
+    )
+
+    BasicTextField(
+        modifier = modifier
+            .border(border = border, shape = ChatTheme.shapes.inputField)
+            .clip(ChatTheme.shapes.inputField)
+            .background(ChatTheme.colors.inputBackground)
+            .padding(innerPadding),
+        value = textFieldValue,
+        onValueChange = {
+            textFieldValueState = it
+            if (value != it.text) {
+                onValueChange(it.text)
+            }
+        },
+        textStyle = ChatTheme.typography.body.copy(
+            color = ChatTheme.colors.textHighEmphasis,
+            textDirection = TextDirection.Content,
+        ),
+        cursorBrush = SolidColor(ChatTheme.colors.primaryAccent),
+        decorationBox = { innerTextField -> decorationBox(innerTextField) },
+        maxLines = maxLines,
+        singleLine = maxLines == 1,
+        enabled = enabled,
+        keyboardOptions = keyboardOptions,
+    )
+}
+
+/**
+ * Check if the [TextFieldValue] state represents a UI with the cursor at the end of the input.
+ *
+ * @return True if the cursor is at the end of the input.
+ */
+private fun TextFieldValue.isCursorAtTheEnd(): Boolean {
+    val textLength = text.length
+    val selectionStart = selection.start
+    val selectionEnd = selection.end
+
+    return textLength == selectionStart && textLength == selectionEnd
 }
--- chat module/src/main/kotlin/com/kobez/chatmodule/ui/components/composer/MessageInput.kt
+++ chat module/src/main/kotlin/com/kobez/chatmodule/ui/components/composer/MessageInput.kt
@@ -1,4 +1,99 @@
 package com.kobez.chatmodule.ui.components.composer
 
-class MessageInput {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.unit.dp
+import com.kobez.chatmodule.state.MessageComposerState
+
+/**
+ * Input field for the Messages/Conversation screen. Allows label customization, as well as handlers
+ * when the input changes.
+ *
+ * @param messageComposerState The state of the input.
+ * @param onValueChange Handler when the value changes.
+ * @param onAttachmentRemoved Handler when the user removes a selected attachment.
+ * @param modifier Modifier for styling.
+ * @param maxLines The number of lines that are allowed in the input.
+ * @param keyboardOptions The [KeyboardOptions] to be applied to the input.
+ * @param label Composable that represents the label UI, when there's no input.
+ * @param innerLeadingContent Composable that represents the persistent inner leading content.
+ * @param innerTrailingContent Composable that represents the persistent inner trailing content.
+ */
+@Composable
+public fun MessageInput(
+    messageComposerState: MessageComposerState,
+    onValueChange: (String) -> Unit,
+    // onAttachmentRemoved: (Attachment) -> Unit,
+    modifier: Modifier = Modifier,
+    maxLines: Int = DefaultMessageInputMaxLines,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
+    label: @Composable (MessageComposerState) -> Unit = {
+        DefaultComposerLabel()
+    },
+    innerLeadingContent: @Composable RowScope.() -> Unit = {},
+    innerTrailingContent: @Composable RowScope.() -> Unit = {},
+) {
+    val (value) = messageComposerState
+
+    InputField(
+        modifier = modifier,
+        value = value,
+        maxLines = maxLines,
+        onValueChange = onValueChange,
+        enabled = true,
+        innerPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        keyboardOptions = keyboardOptions,
+        decorationBox = { innerTextField ->
+            Column {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    innerLeadingContent()
+
+                    Box(modifier = Modifier.weight(1f)) {
+                        innerTextField()
+
+                        if (value.isEmpty()) {
+                            label(messageComposerState)
+                        }
+                    }
+
+                    innerTrailingContent()
+                }
+            }
+        },
+    )
+}
+
+/**
+ * The default number of lines allowed in the input. The message input will become scrollable after
+ * this threshold is exceeded.
+ */
+private const val DefaultMessageInputMaxLines = 6
+
+/**
+ * Default input field label
+ */
+@Composable
+internal fun DefaultComposerLabel() {
+    Text(
+        text = "Ask a question about this location",
+        color = Color.Gray,
+    )
 }
